d84022d25e34f000f22a66fdfbadc81a09caf443
Ben Chuan <benchuan0614@gmail.com>
2021-10-02 21:29:32 +0800

    重新調整package.json

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,51 +2,51 @@ import React from 'react'
 
 export default function News() {
   return (
-    <div class="container">
-      <div class="news">
+    <div className="container">
+      <div className="news">
         {/* 標題行 */}
-        <div class="title-row">
+        <div className="title-row">
           {/* 標題 */}
-          <div class="title">News</div>
+          <div className="title">News</div>
           {/* 展示更多按鈕 */}
-          <div class="view">
+          <div className="view">
             <a href="#/">VIEW ALL</a>
           </div>
         </div>
         {/* item-gorup */}
-        <div class="item-group">
+        <div className="item-group">
           {/* item */}
           <a href="#/">
-            <div class="item">
-              <div class="time">
-                <div class="date">2021.09.30</div>
-                <div class="cate">OTHER</div>
+            <div className="item">
+              <div className="time">
+                <div className="date">2021.09.30</div>
+                <div className="cate">OTHER</div>
               </div>
-              <div class="content">
+              <div className="content">
                 <p>ファッション誌「BAILA」レギュラーモデルに決定！</p>
               </div>
             </div>
           </a>
           {/* item */}
           <a href="#/">
-            <div class="item">
-              <div class="time">
-                <div class="date">2021.09.22</div>
-                <div class="cate">OTHER</div>
+            <div className="item">
+              <div className="time">
+                <div className="date">2021.09.22</div>
+                <div className="cate">OTHER</div>
               </div>
-              <div class="content">
+              <div className="content">
                 <p>JAグループお米消費拡大アンバサダーに就任！</p>
               </div>
             </div>
           </a>
           {/* item */}
           <a href="#/">
-            <div class="item">
-              <div class="time">
-                <div class="date">2021.08.30</div>
-                <div class="cate">MEDIA</div>
+            <div className="item">
+              <div className="time">
+                <div className="date">2021.08.30</div>
+                <div className="cate">MEDIA</div>
               </div>
-              <div class="content">
+              <div className="content">
                 <p>映画「ずっと独身でいるつもり？」に出演決定！</p>
               </div>
             </div>
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,30 +2,30 @@ import React from 'react'
 
 export default function Schedule() {
   return (
-    <div class="container">
-      <div class="schedule">
+    <div className="container">
+      <div className="schedule">
         {/* 標題行 */}
-        <div class="title-row">
+        <div className="title-row">
           {/* 標題 */}
-          <div class="title">SCHEDULE</div>
+          <div className="title">SCHEDULE</div>
           {/* 展示更多按鈕 */}
-          <div class="view">
+          <div className="view">
             <a href="#/">VIEW ALL</a>
           </div>
         </div>
         {/* item-gorup */}
-        <div class="item-group">
+        <div className="item-group">
           {/* item */}
           <a href="#/">
-            <div class="item">
-              <div class="date">
-                <div class="day">05</div>
-                <div class="week">TUE</div>
-                <div class="month">2021.10</div>
+            <div className="item">
+              <div className="date">
+                <div className="day">05</div>
+                <div className="week">TUE</div>
+                <div className="month">2021.10</div>
               </div>
-              <div class="content">
-                <div class="cate-time">
-                  <div class="cate">
+              <div className="content">
+                <div className="cate-time">
+                  <div className="cate">
                     <a href="#/">TV</a>
                   </div>
                   <div className="time">18:55～19:25</div>
@@ -36,15 +36,15 @@ export default function Schedule() {
           </a>
           {/* item */}
           <a href="#/">
-            <div class="item">
-              <div class="date">
-                <div class="day">12</div>
-                <div class="week">TUE</div>
-                <div class="month">2021.10</div>
+            <div className="item">
+              <div className="date">
+                <div className="day">12</div>
+                <div className="week">TUE</div>
+                <div className="month">2021.10</div>
               </div>
-              <div class="content">
-                <div class="cate-time">
-                  <div class="cate">
+              <div className="content">
+                <div className="cate-time">
+                  <div className="cate">
                     <a href="#/">MAGAZINE</a>
                   </div>
                 </div>
@@ -54,15 +54,15 @@ export default function Schedule() {
           </a>
           {/* item */}
           <a href="#/">
-            <div class="item">
-              <div class="date">
-                <div class="day">29</div>
-                <div class="week">FRI</div>
-                <div class="month">2021.10</div>
+            <div className="item">
+              <div className="date">
+                <div className="day">29</div>
+                <div className="week">FRI</div>
+                <div className="month">2021.10</div>
               </div>
-              <div class="content">
-                <div class="cate-time">
-                  <div class="cate">
+              <div className="content">
+                <div className="cate-time">
+                  <div className="cate">
                     <a href="#/">OTHER</a>
                   </div>
                 </div>
